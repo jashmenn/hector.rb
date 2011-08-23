d20b9bc1b614be8f4475e17c1fda3ba83c42bfb5
Nate Murray <nate@natemurray.com>
2011-08-22 17:19:17 -0700

    more consistent usage of ordered hash

--- lib/hector/serialize.rb
+++ lib/hector/serialize.rb
@@ -1,5 +1,6 @@
 class Hector
   module Serialize
+    OH = Hector::OrderedHash 
 
     include_package 'me.prettyprint.cassandra.serializers'
     include_package 'me.prettyprint.cassandra.model'
@@ -34,37 +35,36 @@ class Hector
     def h_to_rb(s)
       case s
       when SuperRowsImpl
-        s.inject(Hector::OrderedHash.new) {|acc, x| acc.merge(h_to_rb(x))}
+        s.inject(OH.new) {|acc, x| acc.merge(h_to_rb(x))}
       when SuperRowImpl
-        {s.getKey => 
-          s.getSuperSlice.getSuperColumns.inject(Hector::OrderedHash.new) {|acc, x| 
-            acc.merge(h_to_rb(x)) }}
+        OH.new.merge({s.getKey => 
+                  s.getSuperSlice.getSuperColumns.inject(OH.new) {|acc, x| 
+                                                                        acc.merge(h_to_rb(x)) }})
       when HSuperColumnImpl
-        {s.getName => 
-          s.getColumns.inject(Hector::OrderedHash.new) {|acc, x| 
-            acc.merge(h_to_rb(x)) }}
+        OH.new.merge({s.getName => 
+                  s.getColumns.inject(OH.new) {|acc, x| acc.merge(h_to_rb(x)) }})
       when RowsImpl
         s.inject(Hector::OrderedHash.new) {|acc, x| acc.merge(h_to_rb(x))}
       when RowImpl
-        {s.getKey => h_to_rb(s.getColumnSlice)}
+        OH.new.merge({s.getKey => h_to_rb(s.getColumnSlice)})
       when ColumnSliceImpl
         s.getColumns.inject(Hector::OrderedHash.new) {|acc, x| acc.merge(h_to_rb(x))}
       when HColumnImpl
-        {s.getName => s.getValue}
+        OH.new.merge({s.getName => s.getValue})
       when Fixnum
-        {:count => s}
+        OH.new.merge({:count => s})
       when QueryResultImpl
         h_to_rb(s.get) # {:exec_us (.getExecutionTimeMicro s) # :host (.getHostUsed s)})
       when KeyspaceDefinition
-        {s.getName => 
-          {:replication_factor => s.getReplicationFactor,
-            :strategy => s.getStrategyClass,
-            :strategy_options => s.getStrategyOptions,
-            :cf_defs => s.getCfDefs}}
+        OH.new.merge({s.getName => 
+                 {:replication_factor => s.getReplicationFactor,
+                  :strategy => s.getStrategyClass,
+                  :strategy_options => s.getStrategyOptions,
+                  :cf_defs => s.getCfDefs}})
       when ColumnFamilyDefinition
-        {:name => s.getName,
-         :comparator => parse_comparator(s.getComparatorType),
-         :type => parse_column_type(s.getColumnType)}
+        OH.new.merge({:name => s.getName,
+                :comparator => parse_comparator(s.getComparatorType),
+                :type => parse_column_type(s.getColumnType)})
       else
         raise "Unknown type #{s} (#{s.class}) for h_to_rb conversion"
       end
